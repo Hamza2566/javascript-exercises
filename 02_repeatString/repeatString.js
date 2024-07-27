612fd01eab2beef4568781598dec26daf546370e
Hamza2566 <140892776+Hamza2566@users.noreply.github.com>
2024-07-27 22:35:25 +0300

    Update repeatString.js

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,11 @@
 const repeatString = function() {
+  let string = 'Hello';
+  for(i=0;i<5;i++){
+    console.log(string)
+  }
 
 };
+repeatString();
 
 // Do not edit below this line
 module.exports = repeatString;
